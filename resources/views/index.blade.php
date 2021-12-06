60d15e0820e6f35136bd9ca7e02e27aedb94b4ca
Omar Ibañez <omaribanez100@gmail.com>
2021-12-06 13:42:46 -0500

    implementando boostrap y sass

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>BIOA2</title>
 
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
 
     <body class="antialiased">
-        <div
-            class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+            Hola soy una plataforma
         </div>
     </body>
 
