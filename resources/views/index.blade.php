45fcb05e8541096c6937f37985362180001ee5f5
Omar Ibañez <omaribanez100@gmail.com>
2021-12-06 16:31:32 -0500

    formulario

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,69 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>BIOA2</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BIOA2</title>
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
 
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<body>
+    <div class="container">
 
-            Hola soy una plataforma
-        </div>
-    </body>
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="inputEmail4">Usuario</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Seleccione...</option>
+                        <option>Ingeniero</option>
+                        <option>Técnico</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputPassword4">Tipo de mantenimiento</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Seleccione...</option>
+                        <option>Preventivo</option>
+                        <option>Correctivo</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="inputAddress">Año</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Seleccione...</option>
+                        <option>2019</option>
+                        <option>2018</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputAddress2">IPC</label>
+                    <input disabled type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="inputCity">Nombre del equipo</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">Serial</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+               
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </form>
+    </div>
+</body>
 
 </html>
