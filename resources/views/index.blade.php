03bf75a03d70d9c71eec83fc18c741e0fd9d235f
Omar Ibanez <omaribanez100@gmail.com>
2021-12-08 10:55:09 -0500

    Mejorando responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -67,11 +67,11 @@
             </div>
         </form>
     </div>
-    <!--<footer class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <footer class="flex-shrink-0 py-4 bg-dark text-white-50">
         <div class="text-center">
           <small>Copyright &copy; Todos los derechos reservados.</small>
         </div>
-    </footer> -->
+    </footer> 
 </body>
 
 </html>
